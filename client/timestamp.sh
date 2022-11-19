78583419308a5e659996d230f0537e28f950bacc
majasma <majasma@stud.ntnu.no>
2022-11-19 12:56:35 +0100

    added shebang and full paths

--- client/timestamp.sh
+++ client/timestamp.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
 # get timestamp
 time=$(date +%s)
 msg=$(date)
 
+
 printf "time in seconds $time\n"
 
 # check if an hour has passed
-if ! (($time % 3600))
+if  [ $(echo "$time % 3600" | bc) -eq 0 ];
 then
     msg="an hour has passed..."
 fi
@@ -17,7 +19,7 @@ printf "message $msg \n"
 url=http://localhost:54321/
 
 # create http request using curl
-(curl -X POST ${url} -H 'Content-type: application/json' --data "{\"text\": \"${msg}\"}" ) >> client.log
-echo "" >> client.log
+(curl -X POST ${url} -H 'Content-type: application/json' --data "{\"text\": \"${msg}\"}" ) >> /home/majasma/echo_timestamp/client/client.log
+echo "" >> /home/majasma/echo_timestamp/client/client.log
 
 
